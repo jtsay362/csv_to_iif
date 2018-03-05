3d8d0a46d0fb3021c74611bc8952556b57dae563
Jeff Tsay <jctsay@aim.com>
2018-03-04 23:25:27 -0800

    Use python 3, be more generic for CSV formats
    Only US Bank Checking verified to work

--- convert.py
+++ convert.py
@@ -7,32 +7,74 @@ import sys, traceback, re
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+HEADER_TEMPLATE = "!ACCNT\tNAME\tACCNTTYPE\tDESC\tACCNUM\tEXTRA\r\n"\
+                 + "ACCNT\t{account}\tBANK\r\n"\
+                 + "ACCNT\tBusiness Misc. Expense\tEXP\r\n"\
+                 + "!TRNS\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
+                 + "!SPL\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
+                 + "!ENDTRNS\r\n"
+
+
+#output_file.write(template.format(transaction_type, date, account, name, amount, memo,
+#                                  transaction_type, date, name, -amount, memo))
+
+
+FORMAT_DESCRIPTORS = {
+  'usbank_checking': {
+    'csv_columns' : ['date', 'transaction', 'name', 'memo', 'amount'],
+    'transaction': "TRNS\t{transaction_type}\t{date}\t{account}\t{name}\t{amount:.2f}\t{memo}\tN\r\n"\
+                   + "SPL\t{transaction_type}\t{date}\tBusiness Misc. Expense\t{name}\t{negative_amount:.2f}\t{memo}\tN\r\n"\
+                   + "ENDTRNS\r\n",
+    'is_credit_account' : False
+  },
+  'capitalone_credit_card': {
+    'csv_columns' : ['date', 'posted_date', 'last4', 'name', 'memo', 'amount'],
+    'transaction': "TRNS\t{transaction_type}\t{date}\t{account}\t{name}\t{amount:.2f}\t{memo}\tN\r\n"\
+                   + "SPL\t{transaction_type}\t{date}\tBusiness Misc. Expense\t{name}\t{negative_amount:.2f}\t{memo}\tN\r\n"\
+                   + "ENDTRNS\r\n",
+    'is_credit_account' : True
+  }
+
+}
+
 
 def error(trans):
   sys.stderr.write("%s\n" % trans)
   traceback.print_exc(None, sys.stderr)
 
 def main(input_file_name):
-  print "Converting %s ..." % input_file_name
+  print("Converting {} ...".format(input_file_name))
   input_file = open(os.path.join(PROJECT_ROOT, input_file_name), 'r')
   #output_file = open(os.path.join(PROJECT_ROOT, input_file_name + '.iif'), 'w')
-  output_file = open('/mnt/Pool1/backup-src/usbank_checking_2017.iif', 'w')
+
+  output_filename = '/mnt/Pool1/backup-src/' + input_file_name + '.iif'
+  #output_filename = os.path.join(PROJECT_ROOT, input_file_name + '.iif')
+
+  print("Output filename = {}".format(output_filename))
+
+  output_file = open(output_filename, 'w')
 
   # This is the name of the QuickBooks checking account
   account = 'US Bank Checking'
+  descriptor = FORMAT_DESCRIPTORS['usbank_checking']
+  csv_columns = descriptor['csv_columns']
+  template = descriptor['transaction']
+  is_credit_account = descriptor['is_credit_account']
 
   # This is the IIF template
 
   #head = "!TRNS	TRNSID	TRNSTYPE	DATE	ACCNT	NAME	CLASS	AMOUNT	DOCNUM	MEMO	CLEAR	TOPRINT	NAMEISTAXABLE	DUEDATE	TERMS	PAYMETH	SHIPVIA	SHIPDATE	REP	FOB	PONUM	INVMEMO	ADDR1	ADDR2	ADDR3	ADDR4	ADDR5	SADDR1	SADDR2	SADDR3	SADDR4	SADDR5	TOSEND	ISAJE	OTHER1	ACCTTYPE	ACCTSPECIAL\r\n"\
   #       + "!SPL	SPLID	TRNSTYPE	DATE	ACCNT	NAME	CLASS	AMOUNT	DOCNUM	MEMO	CLEAR	QNTY	PRICE	INVITEM	PAYMETH	TAXABLE	EXTRA	VATCODE	VATRATE	VATAMOUNT	VALADJ	SERVICEDATE	TAXCODE	TAXRATE	TAXAMOUNT	TAXITEM	OTHER2	OTHER3	REIMBEXP	ACCTTYPE	ACCTSPECIAL	ITEMTYPE\r\n"\
   #+ "!ENDTRNS\r\n"
-  head = "!ACCNT\tNAME\tACCNTTYPE\tDESC\tACCNUM\tEXTRA\r\n"\
-  + "ACCNT\t%s\tBANK\r\n" % (account)
+  # head = "!ACCNT\tNAME\tACCNTTYPE\tDESC\tACCNUM\tEXTRA\r\n"\
+  # + "ACCNT\t{}\tBANK\r\n".format(account)
+  #
+  # head += "ACCNT\tBusiness Misc. Expense\tEXP\r\n"\
+  # + "!TRNS\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
+  # + "!SPL\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
+  # + "!ENDTRNS\r\n"
 
-  head += "ACCNT\tBusiness Misc. Expense\tEXP\r\n"\
-  + "!TRNS\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
-  + "!SPL\tTRNSTYPE\tDATE\tACCNT\tNAME\tAMOUNT\tMEMO\tCLEAR\r\n"\
-  + "!ENDTRNS\r\n"
+  head = HEADER_TEMPLATE.format(account=account)
 
   output_file.write(head)
 
@@ -40,9 +82,9 @@ def main(input_file_name):
   #           + "SPL		CHECK	%s	Ask My Accountant			%s				0	%s							0.00					0.00					EXP\r\n"\
   #+ "ENDTRNS\r\n"
 
-  template = "TRNS\t%s\t%s\t%s\t%s\t%s\t%s\tN\r\n"\
-  + "SPL\t%s\t%s\tBusiness Misc. Expense\t%s\t%s\t%s\tN\r\n"\
-  + "ENDTRNS\r\n"
+  # template = "TRNS\t{}\t{}\t{}\t{}\t{}\t{}\tN\r\n"\
+  # + "SPL\t{}\t{}\tBusiness Misc. Expense\t{}\t{}\t{}\tN\r\n"\
+  # + "ENDTRNS\r\n"
 
   found_header = False
 
@@ -55,36 +97,53 @@ def main(input_file_name):
           found_header = True
           continue
 
-        try:
-          (date, debit_or_credit, name, memo, amount) = row
-        #            date = date.replace('/', '-')
-        except:
-          error(str(row))
-          continue
+        print(row)
 
-        try:
-          amount = float(amount)
-        except:
-          error(str(row))
-          continue
+        data = {
+          'account': account
+        }
 
         transaction_type = 'DEPOSIT'
 
-        if amount < 0:
-          transaction_type = 'CHECK'
+        try:
+          for i, field in enumerate(csv_columns):
+            x = row[i]
+
+            if field == 'amount':
+              amount = x = float(x)
+
+              if is_credit_account:
+                amount = -amount
+
+              data['negative_amount'] = -amount
+
+              if amount < 0:
+                transaction_type = 'CHECK'
+            else:
+              x = x.strip()
+
+            data[field] = x
+        except:
+          error(str(row))
+          continue
+
 
         # template = "TRNS\tCHECK\t3/1/2018\tUS Bank Checking\tSome Junky Expense\t-10000\tThis is a check\tN\r\n"\
         # + "SPL\tCHECK\t3/1/2018\tBusiness Misc. Expense\tSome Junky Expense\t10000\tThis is a check\tN\r\n"\
         # + "ENDTRNS\r\n"
 
-        output_file.write(template % (transaction_type, date, account, name, amount, memo,
-                                      transaction_type, date, name, -amount, memo))
+        data['transaction_type'] = transaction_type
 
-  print "Done."
+        print("data = " + str(data))
+
+
+        output_file.write(template.format(**data))
+
+  print('Done.')
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print "usage:   python convert.py input.csv"
+  if len(sys.argv) != 2:
+      print("usage:   python convert.py input.csv")
 
-    main(sys.argv[1])
+  main(sys.argv[1])
